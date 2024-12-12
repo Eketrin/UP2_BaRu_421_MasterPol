962949dd4d0d63272dac86890537bec0a4b468cb
217000 <217000@KB37-316-C07>
2024-12-12 15:35:34 +0300

    Добавьте файлы проекта.

---
+++ UP2_BaRu_421_MasterPol/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UP2_BaRu_421_MasterPol
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MainFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            if (!(e.Content is Page page)) return;
+            this.Title = $"ProjectByRutBai - {page.Title}";
+
+            if (page is Pages.MenuPage)
+                ButtonBack.Visibility = Visibility.Hidden;
+            else
+                ButtonBack.Visibility = Visibility.Visible;
+
+        }
+
+        private void ButtonBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack) MainFrame.GoBack();
+        }
+    }
+}
